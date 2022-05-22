75a2635c9ea0a527c0c30fd652c953d5624d3837
weilongxing <48667160+weilongxing@users.noreply.github.com>
2022-05-22 18:23:47 +0800

    Add files via upload

---
+++ yygh_parent/hospital-manage/src/main/java/com/atsober/hospitalmanage/mapper/ScheduleMapper.java
@@ -0,0 +1,10 @@
+package com.atsober.hospitalmanage.mapper;
+
+import com.atsober.hospitalmanage.model.Schedule;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScheduleMapper extends BaseMapper<Schedule> {
+
+}
